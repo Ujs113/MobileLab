9b302fc4dbbbaf22075ef2bfad97122275181286
Ujs113 <54476632+Ujs113@users.noreply.github.com>
2022-09-20 15:07:47 +0530

    Added index to multiple exercises
    Made the default landing page into a list view which links to each of the exercises. Also separated the widgets into multiple dart files according to exercise.

---
+++ lib/exercise_1.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+
+class UserForm extends StatefulWidget {
+  const UserForm({Key? key}) : super(key: key);
+
+  @override
+  State<UserForm> createState() => _UserFormState();
+}
+
+String? nullChecker(value) {
+  if(value == null || value.isEmpty){
+    return 'This is a required field';
+  }
+  return null;
+}
+
+String? emailChecker(value){
+  final RegExp regex = RegExp(r"[a-zA-Z0-9]+@[a-zA-Z]+");
+  if(!regex.hasMatch(value))
+  {
+    return '\nEnter a valid email';
+  }
+  return null;
+}
+
+bool isNumeric(String s) {
+  return double.tryParse(s) != null;
+}
+
+String? numberChecker(value) {
+  if(value == null || value.isEmpty){
+    return 'This is a required field';
+  }
+  else if(!isNumeric(value))
+  {
+    return 'Please enter a number';
+  }
+  return null;
+}
+
+class _UserFormState extends State<UserForm> {
+
+  final _formKey = GlobalKey<FormState>();
+  final usernameController = TextEditingController();
+  final emailController = TextEditingController();
+  final registerController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Exercise 1'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.only(
+              top: 32.0,
+              left: 32.0,
+              right: 32.0
+          ),
+          child: Column(children: <Widget> [
+            const Text('User Details', style: TextStyle(fontSize: 20)),
+            TextFormField(
+              controller: usernameController,
+              decoration: const InputDecoration(labelText: 'Name'),
+              validator: nullChecker,
+            ),
+            TextFormField(
+              controller: emailController,
+              decoration: const InputDecoration(labelText: 'E- mail'),
+              validator: emailChecker,
+            ),
+            TextFormField(
+              controller: registerController,
+              decoration: const InputDecoration(labelText: 'Registration Number'),
+              validator: numberChecker,
+            ),
+            Center(
+              child: ElevatedButton(onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) =>
+                      Display(username: usernameController.text,
+                          email: emailController.text, reg: registerController.text)));
+                }
+              }, child: const Text('Submit')),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+class Display extends StatelessWidget {
+  final String username, email, reg;
+  Display({Key? key, required this.username, required this.email, required this.reg}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User Details'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget> [
+            Text(
+              username,
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.black,
+              ),
+            ),
+            Text(
+              email,
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.black,
+              ),
+            ),
+            Text(
+              reg,
+              style: const TextStyle(
+                fontSize: 16,
+                color: Colors.black,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'exercise_1.dart';
+import 'exercise_2.dart';
+
 void main() => runApp(const HelloWorld());
 
 class HelloWorld extends StatelessWidget {
@@ -7,7 +10,7 @@ class HelloWorld extends StatelessWidget {
 
 	@override
 	Widget build(BuildContext context) {
-		const appTitle = 'Hello World App';
+		const appTitle = 'Mobile App Development Lab';
 
 		return MaterialApp(
 			title: appTitle,
@@ -15,126 +18,37 @@ class HelloWorld extends StatelessWidget {
 				appBar: AppBar(
 					title: const Text(appTitle),
 				),
-				body: const UserForm(),
+				body: const Index(),
 			)
 		);
 	}
 }
 
-class UserForm extends StatefulWidget {
-	const UserForm({Key? key}) : super(key: key);
+List exercises = [['Exercise 1', UserForm()], ['Exercise 2', Exercise2()]];
 
-	@override
-	State<UserForm> createState() => _UserFormState();
+class Index extends StatefulWidget {
+  const Index({Key? key}) : super(key: key);
+
+  @override
+  State<Index> createState() => _IndexState();
 }
 
-String? nullChecker(value) {
-	if(value == null || value.isEmpty){
-		return 'This is a required field';
-	}
-	return null;
-}
-
-String? emailChecker(value){
-	final RegExp regex = RegExp(r"[a-zA-Z0-9]+@[a-zA-Z]+");
-	if(!regex.hasMatch(value))
-	{
-		return '\nEnter a valid email';
-	}
-	return null;
-}
-
-bool isNumeric(String s) {
-	return double.tryParse(s) != null;
-}
-
-String? numberChecker(value) {
-	if(value == null || value.isEmpty){
-		return 'This is a required field';
-	}
-	else if(!isNumeric(value))
-	{
-			return 'Please enter a number';
-	}
-	return null;
-}
-
-class _UserFormState extends State<UserForm> {
-
-    final _formKey = GlobalKey<FormState>();
-	final usernameController = TextEditingController();
-	final emailController = TextEditingController();
-	final registerController = TextEditingController();
-
-    @override
-    Widget build(BuildContext context) {
-		return Form(
-			key: _formKey,
-			child: Column(children: <Widget> [
-				TextFormField(
-					controller: usernameController,
-					decoration: const InputDecoration(labelText: 'Name'),
-					validator: nullChecker,
-				),
-				TextFormField(
-					controller: emailController,
-					decoration: const InputDecoration(labelText: 'E- mail'),
-					validator: emailChecker,
-				),
-				TextFormField(
-					controller: registerController,
-					decoration: const InputDecoration(labelText: 'Registration Number'),
-					validator: numberChecker,
-				),
-				ElevatedButton(onPressed: () {
-					if (_formKey.currentState!.validate()) {
-						Navigator.push(context, MaterialPageRoute(builder: (context) =>
-								Display(username: usernameController.text,
-										email: emailController.text, reg: registerController.text)));
+class _IndexState extends State<Index> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+			padding: const EdgeInsets.all(16.0),
+			itemCount: exercises.length,
+			itemBuilder: (context, i) {
+				return GestureDetector(
+					child: ListTile(title: Text(exercises[i][0])),
+					onTap: () {
+						Navigator.push(context, MaterialPageRoute(
+							builder: (context) => exercises[i][1]
+						));
 					}
-				}, child: const Text('Submit')),
-			]),
+				);
+			},
 		);
-    }
-}
-
-class Display extends StatelessWidget {
-	final String username, email, reg;
-	Display({Key? key, required this.username, required this.email, required this.reg}) : super(key: key);
-
-	@override
-	Widget build(BuildContext context) {
-		return Scaffold(
-			appBar: AppBar(
-				title: const Text('User Details'),
-			),
-			body: Center(
-				child: Column(
-					children: <Widget> [
-						Text(
-							username,
-							style: const TextStyle(
-								fontSize: 16,
-								color: Colors.black,
-							),
-						),
-						Text(
-							email,
-							style: const TextStyle(
-								fontSize: 16,
-								color: Colors.black,
-							),
-						),
-						Text(
-							reg,
-							style: const TextStyle(
-								fontSize: 16,
-								color: Colors.black,
-							),
-						)
-					],
-				),
-			),
-		);
-	}
+  }
 }
